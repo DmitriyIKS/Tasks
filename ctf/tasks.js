c9d918aff583c8b4f8923f82a948910a3e6d876e
dmiriyiks <ssseryyy.ru@gmail.com>
2023-04-22 09:57:19 +0300

    Update to 1.2

--- ctf/tasks.js
+++ ctf/tasks.js
@@ -11,10 +11,13 @@ function check_answer1() {
     sub1 = document.getElementById('in1').value
     hex636f7272656374 = lmaojsdlfgsdflgh(hex7461736b31)
     if (sub1 == hex636f7272656374){
-        document.getElementById('res1').innerHTML = lmaojsdlfgsdflgh('Y29ycmVjdCE=')   
+        document.getElementById('res1').innerHTML = lmaojsdlfgsdflgh('Y29ycmVjdCE=')
+        document.getElementById('btn01').style.background = 'green' 
     }
     else{
         document.getElementById('res1').innerHTML = lmaojsdlfgsdflgh('dW5jb3JyZWN0IQ==')
+        document.getElementById('btn01').style.background = 'red'
+        setTimeout(default_color, 2500, 'btn01')
     }
 }
 
@@ -23,9 +26,12 @@ function check_answer2() {
     hex636f7272656374 = lmaojsdlfgsdflgh(hex7461736b32)
     if (sub2 == hex636f7272656374){
         document.getElementById('res2').innerHTML = lmaojsdlfgsdflgh('Y29ycmVjdCE=') 
+        document.getElementById('btn02').style.background = 'green'
     }
     else{
         document.getElementById('res2').innerHTML = lmaojsdlfgsdflgh('dW5jb3JyZWN0IQ==')
+        document.getElementById('btn02').style.background = 'red'
+        setTimeout(default_color, 2500, 'btn02')
     }
 }
 
@@ -33,10 +39,13 @@ function check_answer3() {
     sub3 = document.getElementById('in3').value
     hex636f7272656374 = lmaojsdlfgsdflgh(hex7461736b33)
     if (sub3 == hex636f7272656374){
-        document.getElementById('res3').innerHTML = 'CORRECT!'
+        document.getElementById('res3').innerHTML = lmaojsdlfgsdflgh('Y29ycmVjdCE=') 
+        document.getElementById('btn03').style.background = 'green'
     }
     else{
-        document.getElementById('res3').innerHTML = 'UNCORRECT!'
+        document.getElementById('res3').innerHTML = lmaojsdlfgsdflgh('dW5jb3JyZWN0IQ==')
+        document.getElementById('btn03').style.background = 'red'
+        setTimeout(default_color, 2500, 'btn03')
     }
 }
 
@@ -46,9 +55,12 @@ function check_answer4() {
     hex636f7272656374 = lmaojsdlfgsdflgh(hex7461736b34)
     if (sub4 == hex636f7272656374){
         document.getElementById('res4').innerHTML = lmaojsdlfgsdflgh('Y29ycmVjdCE=')
+        document.getElementById('btn04').style.background = 'green'
     }
     else{
         document.getElementById('res4').innerHTML = lmaojsdlfgsdflgh('dW5jb3JyZWN0IQ==')
+        document.getElementById('btn04').style.background = 'red'
+        setTimeout(default_color, 2500, 'btn04')
     }
 }
 
@@ -57,9 +69,12 @@ function check_answer5() {
     hex636f7272656374 = lmaojsdlfgsdflgh(hex7461736b35)
     if (sub5 == hex636f7272656374){
         document.getElementById('res5').innerHTML = lmaojsdlfgsdflgh('Y29ycmVjdCE=')
+        document.getElementById('btn05').style.background = 'green'
     }
     else{
         document.getElementById('res5').innerHTML = lmaojsdlfgsdflgh('dW5jb3JyZWN0IQ==')
+        document.getElementById('btn05').style.background = 'red'
+        setTimeout(default_color, 2500, 'btn05')
     }
 }
 
@@ -69,9 +84,12 @@ function check_answer6() {
     hex636f7272656374 = lmaojsdlfgsdflgh(hex7461736b36)
     if (sub6 == hex636f7272656374){
         document.getElementById('res6').innerHTML = lmaojsdlfgsdflgh('Y29ycmVjdCE=')
+        document.getElementById('btn06').style.background = 'green'
     }
     else{
         document.getElementById('res6').innerHTML = lmaojsdlfgsdflgh('dW5jb3JyZWN0IQ==')
+        document.getElementById('btn06').style.background = 'red'
+        setTimeout(default_color, 2500, 'btn06')
     }
 }
 
@@ -86,4 +104,9 @@ function lmaojsdlfgsdflgh(str) {
     return decodeURIComponent(atob(str).split('').map(function(c) {
         return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
     }).join(''));
+}
+
+
+function default_color(id){
+    document.getElementById(id).style.background = '#a8a8a8'
 }
